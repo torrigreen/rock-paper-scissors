df797e11d1490fa890905268ff01a7b0965bd6b9
torrigreen <tgreen14@tulane.edu>
2024-01-13 15:34:25 -0600

    Write psuedocode
    Plan out the project implementation and necessary functions

---
+++ script.js
@@ -0,0 +1,20 @@
+// function to randomly return `rock`, `paper`, or `scissors`
+  // have a list of choices
+  // pick a random choice
+  // return the result
+// function to get the player's choice
+  // repeat until the player chooses a valid option
+    // prompt the player for a choice
+    // make the choice lowercase
+    // check for spelling
+    // tell player if their choice was invalid
+  // return the choice
+// function to play a single round
+  // check for a tie
+  // decide who wins if there's no tie
+  // tell the player who won
+// function to play a whole best-of-five game
+  // play a round
+  // add score to whoever wins
+  // play rounds until someone gets to three wins
+  // tell player who wins the game
