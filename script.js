6ff5c4e8ecd858cb207194e1ed67c2d4b162b46d
torrigreen <tgreen14@tulane.edu>
2024-01-14 07:39:28 -0600

    Remove redundant comments
    Some comments were unnecessary and made the code less readable

--- script.js
+++ script.js
@@ -1,46 +1,34 @@
-// function to randomly return `rock`, `paper`, or `scissors`
 function getComputerChoice() {
-  // have a list of choices
   let options = [`rock`, `paper`, `scissors`];
 
-  // pick a random choice
   choice = options[Math.floor(Math.random() * options.length)];
 
-  // return the result
   return choice;
 }
 
-// function to get the player's choice
 function getPlayerChoice () {
-  // repeat until the player chooses a valid option
   let validChoice;
   let choice;
 
   while (!validChoice) {
-    // prompt the player for a choice
     choice = prompt(`rock, paper, or scissors?`);
 
-    // make the choice lowercase
+    // account for the input not matching case
     choice = choice.toLowerCase();
 
-    // check for spelling
+    // check for spelling errors
     let options = [`rock`, `paper`, `scissors`];
     validChoice = options.includes(choice);
 
-    // tell player if their choice was invalid
     if (!validChoice) {alert(`${choice} is not a valid option`)};
   }
 
-  // return the choice
   return choice;
 }
 
-// function to play a single round
 function playRound(playerChoice, computerChoice) {
-  // check for a tie
   if (playerChoice === computerChoice) {return `tie`};
 
-  // decide who wins
   let playerWin = false;
 
   switch (true) {
@@ -51,36 +39,28 @@ function playRound(playerChoice, computerChoice) {
       break;
   }
 
-  // return whether the player won or not
   return playerWin;
 }
 
-// function to play a whole best-of-five game
 function game() {
-  // make variables to track score
   let playerScore = 0;
   let computerScore = 0;
 
-  // play rounds until someone gets to three wins
   while (playerScore < 3 && computerScore < 3) {
-    // play a round
     let result = playRound(getPlayerChoice(), getComputerChoice());
     
-    // alert if there was a tie
     if (result === `tie`) {
       console.log(`you tied! replay the round.`);
-    } // add score to player if they won
-    else if (result) {
+    } else if (result) {
       playerScore++;
       console.log(`you win! score: ${playerScore} - ${computerScore}`);
-    } // add score to computer otherwise
-    else {
+    } else {
       computerScore++;
       console.log(`you lose! score: ${playerScore} - ${computerScore}`);
     }
   }
 
-  // tell player who wins the game
-  let message = (playerScore === 3) ? `you won the game! congratulations!` : `you lost the game! that's too bad!`;
+  let message = (playerScore === 3) ? `you won the game! congratulations!` : 
+                                      `you lost the game! that's too bad!`;
   console.log(message);
 }
